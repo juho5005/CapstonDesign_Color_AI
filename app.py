619291253ef12037bc0690fc7213c98515e177d4
juho <juho5005@dongguk.edu>
2023-05-31 22:20:28 +0900

    'commit'

--- app.py
+++ app.py
@@ -3,100 +3,136 @@ import flask
 import urllib.request
 from PIL import Image, ImageOps
 import numpy as np
+import ast
 
+# Use app with Flask Server
 app = flask.Flask(__name__)
-model = None
+
+# Predefine third_class_names, third_model
+'''
+0 red
+1 green
+2 yellow
+3 blue
+'''
+third_class_names = None
+third_model = None
 
 
+# run model
 def load_model():
-    global model
-    model = tensorflow.keras.models.load_model('keras_model.h5', compile=False)
+    global third_class_names, third_model
+
+    third_class_names = open('labels.txt', 'r').readlines()
+    third_model = tensorflow.keras.models.load_model('keras_model.h5', compile=False)
 
 
-@app.route("/")
-def hello() :
-        return "<img src='http://dn-m.talk.kakao.com/talkm/bl5YbDa3GCC/KbUKUw904pQpPcQytFNoTK/i_aa21d62d4558.png' alt='이미지'>"
+# Main home page
+@app.route('/')
+def main():
+    return "<h1>안녕</h1>"
 
 
+# chatbot api
 @app.route("/api/predict", methods=["POST"])
 def api_predict():
+    global third_class_names, third_model
 
     # UserRequest 중 발화를 req에 parsing.
     req = flask.request.get_json()
     req = req['userRequest']['utterance']
-    print(req)
+    print(f'사용자의 발화가 들어왔을 때를 나타냄 : {req}')
 
-    class_names = open('labels.txt', 'r').readlines()
+    body = flask.request.get_json()
 
+    image_with_information = body['action']['params']['secureimage']
+    print(f"req['action']['params']['secureimage'] : {image_with_information}")
+    print()
 
+    user_id = body['userRequest']['user']['id']
+    print(f"user_id : {user_id}")
+    print()
 
-# 이미지 전처리 - 발화가 jpg, png 확장자일 때만 실행
-    if ('jpg' in req) or ('png' in req) or ('jpeg' in req):
-        # np.set_printoptions(suppress=True)
-        data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+    # 문자열로 바꿔주는 코드 (json 형식이라 필히 해야함)
+    image_urls = ast.literal_eval(image_with_information)
+    new_url = image_urls['secureUrls']
 
-        urllib.request.urlretrieve(req, 'img')
-        image = Image.open('img').convert('RGB')
+    saved_image_url = new_url[5:-1]
+    print(f"저장된 최종 이미지 url : {saved_image_url}")
+    print()
 
-        size = (224, 224)
-        image = ImageOps.fit(image, size, Image.ANTIALIAS)
+    np.set_printoptions(suppress=True)
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
-        image_array = np.asarray(image)
+    # 'img'안에 saved_image_url을 넣어줌
+    urllib.request.urlretrieve(saved_image_url, 'img')
 
-        image.show()
+    image = Image.open('img').convert('RGB')
+    size = (224, 224)
+    image = ImageOps.fit(image, size, Image.ANTIALIAS)
+    image_array = np.asarray(image)
+    normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
+    data[0] = normalized_image_array
 
-        normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+    # 모델을 돌리는 곳 이게 좀 오래걸림
+    prediction = third_model.predict(data)
 
-        data[0] = normalized_image_array
-        prediction = model.predict(data)
+    # 가장 확률이 높은 인덱스를 찾아주는 것
+    index = np.argmax(prediction)
 
-        output = np.argmax(prediction, axis=-1)
+    # 가장 확률이 높은 색상을 (index)를 통해 class_name에 넣어주는 것
+    class_name = third_class_names[index]
+    confidence_score = round(prediction[0][index], 2)
 
-        print(output)
+    print(f'빨간색 : {round(prediction[0][0] * 100, 2)}')
+    print(f'초록색 : {round(prediction[0][1] * 100, 2)}')
+    print(f'황색 : {round(prediction[0][2] * 100, 2)}')
+    print(f'청색 : {round(prediction[0][3] * 100, 2)}')
 
-        msg = "빨간색입니다!"
-        print(msg)
+    print("Class : ", class_name[2:], end='')
+    print("Confidence Score : ", confidence_score)
+    print()
 
+    red_perc, green_perc, yellow_perc, blue_perc = \
+    str(round(prediction[0][0] * 100, 2)), str(round(prediction[0][1] * 100, 2)),\
+        str(round(prediction[0][2] * 100, 2)), str(round(prediction[0][3] * 100, 2))
 
-# basic card format
-        res = {
-            "version": "2.0",
-            "template": {
-                "outputs": [
-                    {
-                        "basicCard": {
-                            "title": msg,
-                            "description": "",
-                            "thumbnail": {
-                                "imageUrl": req
-                            },
-                            "buttons": [
-                                {
-                                    "action":  "webLink",
-                                    "label": "사진보기",
-                                    "webLinkUrl": req
-                                }
-                            ]
-                        }
-                    }
-                ]
-            }
-        }
-
+    if str(class_name[2:]).strip() == 'blue':
+        color = '청색'
+    elif str(class_name[2:]).strip() == 'red':
+        color = '빨간색'
+    elif str(class_name[2:]).strip() == 'green':
+        color = "초록색"
     else:
-        # simple text format
-        res = {
-            "version": "2.0",
-            "template": {
-                "outputs": [
-                    {
-                        "simpleText": {
-                            "text": "사진을 보내주세요"
-                        }
+        color = "황색"
+
+    msg = color
+    print(msg)
+
+# Basic Card Format
+    res = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "basicCard": {
+                        "title": "판별 색깔 : " + msg,
+                        "description": "빨강색 : " + red_perc + '%\n' + "초록색 : " + green_perc + '%\n' + "청색 : " + blue_perc + '%\n' + "황색 : " + yellow_perc + '%\n',
+                        "thumbnail": {
+                            "imageUrl": saved_image_url
+                        },
+                        "buttons": [
+                            {
+                                "action":  "webLink",
+                                "label": "전송한 사진 다시 보기",
+                                "webLinkUrl": saved_image_url
+                            }
+                        ]
                     }
-                ]
-            }
+                }
+            ]
         }
+    }
     print(res)
     return flask.jsonify(res)
 
